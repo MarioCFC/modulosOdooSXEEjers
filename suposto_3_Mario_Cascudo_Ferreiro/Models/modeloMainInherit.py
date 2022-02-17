8355c84d8cc1f7025aac156a6ededec4bb766815
Mario Cascudo <mariocscdclase@gmail.com>
2022-02-17 14:55:51 +0100

    Seguimos con el supuesto 3 fata la condicion del notebook

--- suposto_3_Mario_Cascudo_Ferreiro/Models/modeloMainInherit.py
+++ suposto_3_Mario_Cascudo_Ferreiro/Models/modeloMainInherit.py
@@ -1,8 +1,12 @@
 from odoo import models,fields
 class ModeloMainInheritJardineria(models.Model):
     _inherit = 'modelo.main.jardineria'
+
+    tieneSeguro = fields.Boolean('Tiene seguro',required="True")
     compañiaAseguradora = fields.Selection([('a','allianz'),('b','mapfre'),('c','groupama'),('d','zurich')])
-    numeroPoliza = fields.Integer('Numero de póliza', required = True)
-    importeSeguro = fields.Float('Importe', required = True)
-    dataSeguro = fields.Date('Fecha de seguro', required = True)
-    contactoTomador = fields.Char('Contacto de tomador', required = True)
+    numeroPoliza = fields.Integer('Numero de póliza', required = False)
+    importeSeguro = fields.Float('Importe', required = False)
+    dataSeguro = fields.Date('Fecha de seguro', required = False)
+    contactoTomador = fields.Char('Contacto de tomador', required = False)
+
+    telefonoContacto = fields.Integer('Teléfono de contacto', required = False)
--- suposto_3_Mario_Cascudo_Ferreiro/__manifest__.py
+++ suposto_3_Mario_Cascudo_Ferreiro/__manifest__.py
@@ -3,7 +3,7 @@
     'description':'Modulo realizdo en el supuesto 3 donde se hace uso de la herencia',
     'author':'Mario Cascudo',
     'data':[
-
+        'Views/vista.xml'
     ],
     'depends':['suposto_2_Mario_Cascudo_Ferreiro'],
     'application':False
